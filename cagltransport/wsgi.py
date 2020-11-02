a736048c0befe180c18d171aee3580120320a3ef
Gabriel Gilles <gabbygilles1@gmail.com>
2020-11-02 14:04:20 +0000

    working login

--- cagltransport/wsgi.py
+++ cagltransport/wsgi.py
@@ -4,11 +4,7 @@ WSGI config for cagltransport project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-<<<<<<< HEAD
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
-=======
-https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
->>>>>>> 6b4de6aef6f281aa932af2f795ad7357155b6999
 """
 
 import os
